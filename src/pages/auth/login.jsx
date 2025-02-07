748b6f0e2c77a15ff93cef224f3499caaf361633
Isaacjbar <isaac777jb@gmail.com>
2025-02-06 22:42:02 -0600

    feat: Update dynamic load styles in Login

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input } from "antd";
-import { StyleContext } from '../../core/StyleContext';
+import { StyleContext } from "../../core/StyleContext";
 import Strings from "../../utils/localizations/Strings";
 
 export function Login() {
@@ -15,7 +15,10 @@ export function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen" style={{ backgroundColor: style.lightBackgroundColor }}>
+    <div
+      className="flex items-center justify-center h-screen"
+      style={{ backgroundColor: style.BgInterface }}
+    >
       <Form
         className="w-96 shadow-2xl rounded-2xl p-4 pt-8"
         name={Strings.login}
@@ -24,7 +27,7 @@ export function Login() {
         }}
         style={{
           maxWidth: 360,
-          backgroundColor: style.baseColor,
+          backgroundColor: style.BgCard,
           color: "#ffffff",
         }}
         onFinish={onFinish}
@@ -39,9 +42,11 @@ export function Login() {
           ]}
         >
           <Input
-            prefix={<UserOutlined style={{ color: style.lightBackgroundColor }} />}
+            prefix={
+              <UserOutlined style={{ color: style.lightBackgroundColor }} />
+            }
             placeholder={Strings.email}
-            style={{ backgroundColor: style.darkBackgroundColor, color: "#ffffff" }}
+            style={{ backgroundColor: style.BgInterface, color: style.H3 }}
           />
         </Form.Item>
 
@@ -55,28 +60,32 @@ export function Login() {
           ]}
         >
           <Input
-            prefix={<LockOutlined style={{ color: style.lightBackgroundColor }} />}
+            prefix={
+              <LockOutlined style={{ color: style.lightBackgroundColor }} />
+            }
             type={Strings.password}
             placeholder={Strings.password}
-            style={{ backgroundColor: style.darkBackgroundColor, color: "#ffffff" }}
+            style={{ backgroundColor: style.BgInterface, color: style.H3 }}
           />
         </Form.Item>
 
         <Form.Item>
           <div className="flex justify-between items-center">
             <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox style={{ color: "#ffffff" }}>Remember me</Checkbox>
+              <Checkbox style={{ color: style.H3 }}>Remember me</Checkbox>
             </Form.Item>
-            <a href="" style={{ color: "#ffffff"}}>Forgot password</a>
+            <a href="" style={{ color: style.H3 }}>
+              Forgot password
+            </a>
           </div>
         </Form.Item>
 
         <Form.Item>
-          <Button 
-            block 
-            type="primary" 
-            htmlType="submit" 
-            style={{ backgroundColor: style.mediumBackgroundColor, borderColor: style.mediumBackgroundColor }}
+          <Button
+            block
+            type="primary"
+            htmlType="submit"
+            style={{ background: style.BgButton, color: style.P }}
           >
             Log in
           </Button>
